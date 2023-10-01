db43751d95918eb77aba98dd0e00dc8c9f8212d6
ori268 <ojoj4055@gmail.com>
2023-10-01 13:32:26 +0900

    수정

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
 		Rectangle re = new Rectangle ();
 		Triangle tr = new Triangle ();
 		
-		re.setLength(12);
+		re.setLength(23);
 		re.setWidth(5);
 		
 		tr.setBase(5);
@@ -14,6 +14,8 @@ public class Test {
 		
 		System.out.println(re.toString());
 		System.out.println(tr.toString());
+		System.out.println(tr.toString());
+
 	}
 	
 }
